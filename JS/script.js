43536e900226c18770fbc1f1fba3ddd6d08e085f
Maciej Pałubski <maciejpalubski@gmail.com>
2022-11-20 19:06:36 +0100

    remove task added

--- JS/script.js
+++ JS/script.js
@@ -8,19 +8,39 @@
         for (const task of tasks) {
             htmlString += `
             <li>
-                ${task.content}
+                <span>${task.content}</span>
+                <button class="js-remove">🗑</button>
             </li>
             `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+       
     }
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    }
+
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
 
         render();
-    }
+    };
+
+    
+       
+
+    
 
     const init = () => {
         render();
@@ -38,7 +58,6 @@
 
               addNewTask(newTaskContent)
         });
-
 
 
 
